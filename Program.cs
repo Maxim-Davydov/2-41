1db1af5eb17219138a107bd08f2968f3239aa1e6
Maxim-Davydov <116034877+Maxim-Davydov@users.noreply.github.com>
2023-02-15 13:12:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        double angh, angm, hour, min;
+        Console.WriteLine("Введите значение угла y");
+        angh = double.Parse(Console.ReadLine());
+        if (angh >= 0 && angh <= 360)
+        {
+            angm = angh % 30;
+            hour = (angh - angm) / 30;
+            angm = angm * 2;
+            min = angm / 6;
+            Console.WriteLine($"Градус минутной стрелки={angm},Количество полных часов= {hour},Количество полных минут={min}");
+        }
+        else
+        {
+            Console.WriteLine("Введённые данные не верные");
+        }
+    }
+}
